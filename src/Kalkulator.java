e7be7c6e2db4663ef6c57f6c1944c133615f4492
Amel Nurkic <domace-matrice@hotmail.com>
2016-04-22 20:55:06 +0200

    Pocetni dio izrade kalkulatora.

---
+++ src/Kalkulator.java
@@ -0,0 +1,27 @@
+
+public class Kalkulator {
+
+	public static double saberi(double a, double b) {
+		return 0;
+	}
+
+	public static double oduzmi(double a, double b) {
+		return 0;
+	}
+
+	public static double pomnozi(double a, double b) {
+		return 0;
+	}
+
+	public static double podijeli(double a, double b) {
+		return 0;
+	}
+
+	public static double korjenuj(double a, double b) {
+		return 0;
+	}
+
+	public static double kvadriraj(double a, double b) {
+		return 0;
+	}
+}
